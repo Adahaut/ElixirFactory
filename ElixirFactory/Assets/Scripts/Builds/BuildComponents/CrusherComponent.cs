dfec20c87d46f8c11b65af7c7aba2348011c2bc1
Adahaut <adao.vo.t@gmail.com>
2024-10-21 10:26:07 +0200

    Factorise with heritage

--- ElixirFactory/Assets/Scripts/Builds/BuildComponents/CrusherComponent.cs
+++ ElixirFactory/Assets/Scripts/Builds/BuildComponents/CrusherComponent.cs
@@ -1,83 +1,12 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CrusherComponent : MonoBehaviour, BuildInterface
+public class CrusherComponent : BuildProperties
 {
-    private GameObject UI;
-    private bool isRecipeSet = false;
-    private Recipe recipe;
-    private List<Item> toBuildItem;
-    private Item result;
-    
-    private void Start()
-    {
-        toBuildItem = new (1);
-        toBuildItem.Add(new Item());
-        result = new Item();
-    }
-
     private void OnMouseUpAsButton()
     {
         UIReferencer.Instance.crusherUI.SetActive(true);
         UIReferencer.Instance.crusherUI.GetComponent<CrusherUI>().currentCrusher = gameObject;
     }
-
-    public bool GetRecipeSet()
-    {
-        return isRecipeSet;
-    }
-
-    public Item GetResult()
-    {
-        return result;
-    }
-
-    public List<Item> GetToBuildItem()
-    {
-        return toBuildItem;
-    }
     
-    public void SetRecipe(Recipe newRecipe)
-    {
-        recipe = newRecipe;
-        SetItem(newRecipe.result, result);
-        SetItem(newRecipe.toBuildItems[0], toBuildItem[0]);
-        isRecipeSet = true;
-    }
-    
-    public void ConstructItem()
-    {
-        if (toBuildItem[0].currentStack >= recipe.toBuildItems[0].currentStack)
-        {
-            toBuildItem[0].currentStack -= recipe.toBuildItems[0].currentStack;
-            StartCoroutine(ConstructItemCoroutine());
-        }
-    }
-    
-    private IEnumerator ConstructItemCoroutine()
-    {
-        float timer = 0f;
-        while (timer / recipe.buildTime < 1)
-        {
-            timer += Time.deltaTime;
-            UIReferencer.Instance.crusherUI.GetComponent<CrusherUI>().sliderConstructTime.value = timer / recipe.buildTime;
-            yield return new WaitForEndOfFrame();
-        }
-            result.currentStack += recipe.result.currentStack;
-            UIReferencer.Instance.crusherUI.GetComponent<CrusherUI>().sliderConstructTime.value = 0;
-            UIReferencer.Instance.crusherUI.GetComponent<CrusherUI>().outSlot.GetComponent<DragAndDropSlot>()
-                .itemInSolt = result;
-            ConstructItem();
-            yield return null;
-    }
-    
-    private void SetItem(Item newItem, Item itemToSet)
-    {
-        itemToSet.itemIcon = newItem.itemIcon;
-        itemToSet.itemName = newItem.itemName;
-        itemToSet.currentStack = 0;
-        itemToSet.maxstack = newItem.maxstack;
-    }
 }
--- ElixirFactory/Assets/Scripts/Builds/BuildProperties.cs
+++ ElixirFactory/Assets/Scripts/Builds/BuildProperties.cs
@@ -1,20 +1,35 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
-public class BuildProperties : MonoBehaviour
+
+public class BuildProperties : MonoBehaviour, BuildInterface
 {
     public Vector2 coordinates;
     public Vector2 size;
+    private GameObject UI;
+    private bool isRecipeSet = false;
+    private Recipe recipe;
+    private List<Item> toBuildItem;
+    private Item result;
 
+    private void Start()
+    {
+        toBuildItem = new ();
+        result = new Item();
+    }
+    
     public void SetCoordinates(Vector2 newCoordinates)
     {
         coordinates = newCoordinates;
     }
-    
+
     public Vector2 GetCoordinates()
     {
         return coordinates;
     }
-    
+
     public Vector2 GetSize()
     {
         return size;
@@ -30,5 +45,72 @@ public class BuildProperties : MonoBehaviour
             }
         }
     }
+
+    public bool GetRecipeSet()
+    {
+        return isRecipeSet;
+    }
+
+    public Item GetResult()
+    {
+        return result;
+    }
+
+    public List<Item> GetToBuildItem()
+    {
+        return toBuildItem;
+    }
     
+    public void SetRecipe(Recipe newRecipe)
+    {
+        recipe = newRecipe;
+        SetItem(newRecipe.result, result);
+        SetItem(newRecipe.toBuildItems[0], toBuildItem[0]);
+        isRecipeSet = true;
+    }
+    
+    protected void SetItem(Item OldItem, Item itemToSet)
+    {
+        itemToSet.itemIcon = OldItem.itemIcon;
+        itemToSet.itemName = OldItem.itemName;
+        itemToSet.currentStack = 0;
+        itemToSet.maxstack = OldItem.maxstack;
+    }
+
+    protected void SetListOfItems(List<Item> OldItemList, List<Item> ItemToSet)
+    {
+        for (int i = 0; i < ItemToSet.Count; i++)
+        {
+            ItemToSet[i].itemIcon = OldItemList[i].itemIcon;
+            ItemToSet[i].itemName = OldItemList[i].itemName;
+            ItemToSet[i].currentStack = 0;
+            ItemToSet[i].maxstack = OldItemList[i].maxstack;
+        }
+    }
+    
+    public void ConstructItem()
+    {
+        if (toBuildItem[0].currentStack >= recipe.toBuildItems[0].currentStack)
+        {
+            toBuildItem[0].currentStack -= recipe.toBuildItems[0].currentStack;
+            StartCoroutine(ConstructItemCoroutine());
+        }
+    }
+    
+    private IEnumerator ConstructItemCoroutine()
+    {
+        float timer = 0f;
+        while (timer / recipe.buildTime < 1)
+        {
+            timer += Time.deltaTime;
+            UIReferencer.Instance.crusherUI.GetComponent<CrusherUI>().sliderConstructTime.value = timer / recipe.buildTime;
+            yield return new WaitForEndOfFrame();
+        }
+        result.currentStack += recipe.result.currentStack;
+        UIReferencer.Instance.crusherUI.GetComponent<CrusherUI>().sliderConstructTime.value = 0;
+        UIReferencer.Instance.crusherUI.GetComponent<CrusherUI>().outSlot.GetComponent<DragAndDropSlot>()
+            .itemInSolt = result;
+        ConstructItem();
+        yield return null;
+    }
 }
